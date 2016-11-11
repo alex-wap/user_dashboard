9d5a5c24d2065167a7d049054301b786f6b54b26
Alex W <wapniarski@gmail.com>
2016-11-10 23:00:21 -0800

    changed host to localhost

--- app/config/database.py
+++ app/config/database.py
@@ -15,7 +15,7 @@ class DevelopmentDBConfig(DBConfig):
     DB_USERNAME = 'bafc34af816ab9'
     DB_PASSWORD = '350f9484'
     DB_DATABASE_NAME = 'dashboard'
-    DB_HOST = '127.0.0.1'
+    DB_HOST = 'localhost'
     DB_PORT = 3306
     # """ unix_socket is used for connecting with MAMP. Take this out if you aren't using MAMP """
     DB_OPTIONS = {
@@ -27,11 +27,11 @@ class StagingDBConfig(DBConfig):
     DB_USERNAME = 'bafc34af816ab9'
     DB_PASSWORD = '350f9484'
     DB_DATABASE_NAME = 'dashboard'
-    DB_HOST = '127.0.0.1'
+    DB_HOST = 'localhost'
 
 """ Put Production Specific Configurations here """
 class ProductionDBConfig(DBConfig):
     DB_USERNAME = 'bafc34af816ab9'
     DB_PASSWORD = '350f9484'
     DB_DATABASE_NAME = 'dashboard'
-    DB_HOST = '127.0.0.1'
+    DB_HOST = 'localhost'
